180d01c60140989a02487c1508264bc128a9dac2
inga911 <inga.banaityte221@gmail.com>
2024-02-06 18:46:18 +0100

    404 and 500 pages

---
+++ hotel/resources/views/errors/500.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>500</title>
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/main-front.css') }}">
+    @vite(['resources/scss/main.scss'])
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+    <div id="notfound">
+        <div class="notfound">
+            <div class="notfound-404">
+                <h3>Oops! Page not found</h3>
+                <h1><span>5</span><span>0</span><span>0</span></h1>
+            </div>
+            <h2>we are sorry, but the page you requested was not found</h2>
+        </div>
+    </div>
+</body>
+
+</html>
